c84d82192b21f75c440e2761a491acec7c44fc64
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2020-11-23 21:03:34 -0300

    Estilizando o container de login para os padrões do mockup

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,49 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import logo from '../../assets/logo.png';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+
+import { AppContainer, LoginContainer, Texto } from './styles';
 
 export default function Login({ navigation }) {
     return (
-        <View style={styles.container}>
-            <Image
-                source={logo} />
+        <AppContainer>
+            <LoginContainer>
+                <TouchableOpacity
+                    onPress={() => { navigation.push('Aluno',{ route: 'Cadastro de Aluno' })} }>
+                    <Text>Login como Aluno</Text>
+                </TouchableOpacity>
 
-            <TouchableOpacity
-                style={styles.btn}
-                onPress={() => { navigation.push('Aluno',{ route: 'Cadastro de Aluno' })} }>
-                <Text style={styles.btnText}>Login como Aluno</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                style={styles.btn}
-                onPress={() =>  { navigation.push('Professor', { route: 'Cadastro de Professor' })} }>
-                    <Text style={styles.btnText}>Login como Professor</Text>
-            </TouchableOpacity>
-        </View>
+                <TouchableOpacity
+                    onPress={() =>  { navigation.push('Professor', { route: 'Cadastro de Professor' })} }>
+                        <Text>Login como Professor</Text>
+                </TouchableOpacity>
+            </LoginContainer>
+        </AppContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#eeeeee',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    btn: {
-        borderRadius: 30,
-        backgroundColor: '#1c90ce',
-        width: '70%',
-        alignItems: 'center',
-        paddingTop: 20,
-        paddingBottom: 20,
-        marginTop: 5
-    },
-    btnText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 15
-    }
-});
---
+++ pages/Login/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const AppContainer = styled.View`
+    background-color: #AB1B71;
+    flex: 1;
+    padding: 10px 10px 0px 10px;
+    align-items: center;
+    justify-content: flex-end;
+`;
+
+export const LoginContainer = styled.View`
+    height: 52%;
+    background-color: #FFF;
+    width: 100%;
+    border-top-right-radius: 25px;
+    border-top-left-radius: 25px
+`;
+
+export const Texto = styled.Text`
+
+`;
