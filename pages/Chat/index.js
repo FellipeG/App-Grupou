43649e477d2c9361ca48b1f07555f81d03242e3a
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2020-12-04 01:04:27 -0300

    Chat funcionando

--- pages/Chat/index.js
+++ pages/Chat/index.js
@@ -1,20 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import { UserContext } from '../../contexts/user';
 
 import { 
     AppContainer,
-    Texto } from './styles';
+    Texto,
+    BotaoContainer,
+    BotaoForm,
+    BotaoFormTexto,
+    Input,
+    ContainerMessages,
+    Message } from './styles';
+
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 const Chat = () => {
 
+    const date = new Date();
+
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
     const { user } = useContext(UserContext);
+
+    const updateMessagesListener = (snapshot) => {
+        const data = snapshot.docs.map(document => {
+            return {
+                id: document.id,
+                ...document.data()
+            }
+        })
+
+        setMessages(data);
+    };
+
+    useEffect(() => {
+        const listener = firebase.firestore()
+            .collection('mensagens').orderBy('criado_em', 'asc').onSnapshot(updateMessagesListener)
+    }, []);
+
+    const handleAddMessages = () => {
+        if (newMessage.length === 0) {
+            console.warn('Preencha o campo');
+            return;
+        }
+
+        try {
+            firebase.firestore().collection('mensagens').add({
+                mensagem: newMessage,
+                lida: false,
+                enviado_por: user.email,
+                criado_em: `${date.getHours}${date.getMinutes()}${date.getSeconds()}` 
+            })
+            setNewMessage('');
+        } catch(err) {
+            console.error(err);
+        }
+    }
 
     return (
         <AppContainer>
-            <Texto>{ user.email }</Texto>            
+            <ContainerMessages>
+                { messages.map(element => (<Message key={ element.id }>{ element.mensagem }</Message>)) }
+            </ContainerMessages>
+            <BotaoContainer>
+                <Input
+                    placeholder="Mensagem..."
+                    onChangeText={ text => setNewMessage(text) }
+                    value={ newMessage }
+                />
+                <BotaoForm 
+                    invert={true}
+                    onPress={ () => { handleAddMessages() } }>
+                    <BotaoFormTexto invert={true}>Enviar</BotaoFormTexto>
+                </BotaoForm>
+            </BotaoContainer>          
         </AppContainer>
+
     );
 }
 
--- pages/Chat/styles.js
+++ pages/Chat/styles.js
@@ -3,9 +3,9 @@ import styled from 'styled-components/native';
 export const AppContainer = styled.View`
     background-color: #AB1B71;
     flex: 1;
-    padding: 10px 10px 0px 10px;
+    padding: 25px 10px 0px 10px;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-end;
 `;
 
 export const Texto = styled.Text`
@@ -14,3 +14,45 @@ export const Texto = styled.Text`
     text-align: center;
 `;
 
+export const BotaoContainer = styled.View`
+    flex-direction: row;
+    margin-bottom: 10px;
+`;
+
+export const BotaoForm = styled.TouchableOpacity`
+    height: 60px;
+    flex: 1;
+    align-items: center;
+    justify-content:center;
+    background-color: ${ props => (props.invert == true) ? '#FFF' : '#AE1B73' };
+    border-radius: 5px;
+    border: 1px solid #AE1B73;
+`;
+
+export const BotaoFormTexto = styled.Text`
+    font-size: 16px;
+    color: ${ props => (props.invert == true) ? '#AE1B73' : '#FFF' };
+    font-weight: bold;
+`;
+
+export const Input = styled.TextInput`
+    flex: 3;
+    border: 1px solid #CCC;
+    height: 60px;
+    border-radius: 5px;
+    padding: 0 20px;
+    background-color: #E6E6E6;
+    margin-right: 10px;
+`;
+
+export const ContainerMessages = styled.View`
+    flex: 1;
+    width: 100%;
+`;
+
+export const Message = styled.Text`
+    font-size: 14px;
+    text-align: justify;
+    color: #FFF;
+    padding-top: 5px;
+`;
